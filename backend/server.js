488483279d4da6b7051aef96222a7a84afd6d455
Dan <dan.thick@hotmail.co.uk>
2022-06-14 20:07:09 +0100

    Added Player model

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,22 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
- 
+const Player = require('./models/player')
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+
+// Testing player creation
+playerList = [];
+
+var player = new Player("Dan");
+var player2 = new Player("Jack");
+playerList.push(player);
+playerList.push(player2);
+
+playerList.forEach(element => {
+  console.log(element)
+});
+
