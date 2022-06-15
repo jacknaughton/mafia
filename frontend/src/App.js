49d5fd08a59da1c83fef3bc524ec10b7531db40b
Dan <dan.thick@hotmail.co.uk>
2022-06-15 21:48:49 +0100

    Added functionality to create a new 'game' in the backend via the frontend. Magic...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  createGame() {
+    fetch('/api/game', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true
+        }
+      }
+  ).then(function(res) {
+      res.json().then(log => {
+        
+      });
+      }).catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.createGame}>Create game</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
